5d70e611b46f79d3de932478e5572a30d300d9b8
Nonoo <nonoo@nonoo.hu>
2015-04-24 21:18:07 +0000

    fixed a bug in the update check script which caused the updater script to be run multiple times

--- ha5kdr-dmr-live-update-check.sh
+++ ha5kdr-dmr-live-update-check.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if [ -z "`ps a | grep ha5kdr-dmr-live-update.sh`" ]; then
+if [ -z "`ps ax | grep ha5kdr-dmr-live-update.sh | grep -v grep`" ]; then
 	/home/nonoo/bin/ha5kdr-dmr-live-update.sh &
 fi
